44bb6f8e959b0bb41d373c7c793adb52a28a6b44
lewk764 <114536592+lewk764@users.noreply.github.com>
2022-10-29 15:16:04 +0000

    Finished (I think)

--- main.py
+++ main.py
@@ -46,16 +46,57 @@ def hasWon(secret, guesses):
 
 #Main game code
 
+#===Instructions===#
+print("""
+ __      __              _    ___                
+ \ \    / /__ _ _ _ _ __| |  / __|_  _ ___ ______
+  \ \/\/ / _ \ '_| '_/ _` | | (_ | || / -_|_-<_-<
+   \_/\_/\___/_| |_| \__,_|  \___|\_,_\___/__/__/                       
+""")
+print("Welcome to Word Guess!")
+print("You will be prompted to guess a letter to discover the secret word.")
+print("However, you have 5 strikes! Guess incorrectly and you will gain a strike!")
+print("Good luck!")
+print()
+  
+#===Variables===#
 #SECRET WORD
-secret = "SPHyNX"
+secret = "Spyhnx"
+secret = secret.upper() #Just incase someone changes secret and does not make it all caps
 
 #List of Guesses
 guesses = []
 
-#Number of Tries (default value)
-tries = 0
+#Number of strikes (default value)
+strikes = 0
+
+#===The Game Loop===#
+while strikes < 5 and not hasWon(secret, guesses):
+
+  #Step 1: Prompt for a guess
+  guess = getGuess()
+  guesses.append(guess)
   
-#Test area
-guesses.append(getGuess())
-guesses.append(getGuess())
-displaySecret(secret, guesses)
+  #Step 2: Check if guess is in the secret word
+  if guess in secret:
+    #If the guess is in the secret word, show the user where
+    displaySecret(secret, guesses)
+    print()
+
+  else:
+    #If the guess is not in the secret, add a strike and tell the user
+    strikes += 1
+    print(f"{guess} is not in the word. You have {strikes}/5 strikes.")
+
+#Tell the user if they won or not
+if hasWon(secret, guesses):
+  print("""
+  ◝(ᵔᵕᵔ)◜
+  """)
+  print("Congratulations! You won!")
+else:
+  print("""
+  ( • ᴖ • ｡)
+  """)
+  print("Sorry, better luck next time!")
+
